635afd63313a620538726d15d2e5390941ef6510
Julia Banerjee <juliabanerjee@Julias-MacBook-Pro.local>
2021-11-22 12:54:46 +0000

    getting through making the data sets to loop through

--- main.js
+++ main.js
@@ -1,42 +1,112 @@
-const alphabet = [
-  {letter: 
-  translation: };
-  {letter:
-    translation: };
-  {letter:
-  translation: };
-  {letter:
-  translation: };
-  {letter: 
-  translation: };
-  {letter:
-    translation: };
-  {letter:
-  translation: };
-  {letter:
-  translation: };
-  {letter: 
-  translation: };
-  {letter:
-    translation: };
-  {letter:
-  translation: };
-  {letter:
-  translation: };
-  {letter: 
-  translation: };
-  {letter:
-    translation: };
-  {letter:
-  translation: };
-  {letter:
-  translation: };
-
-
-
-
-
-  
-  
+const letterToMorseCode = [
+  {letter: "a",
+  translation: ".-" },
+  {letter: "b",
+  translation: "-..." },
+  {letter: "c",
+  translation: "-.-." },
+  {letter: "d",
+  translation:  "-.." },
+  {letter: "e",
+  translation: "."},
+  {letter: "f",
+    translation: "..-."},
+  {letter: "g",
+  translation: "--."},
+  {letter: "h",
+  translation: "...."},
+  {letter: "i",
+  translation: ".."},
+  {letter: "j",
+    translation: ".---"},
+  {letter: "k",
+  translation: "-.-"},
+  {letter: "l",
+  translation: ".-.."},
+  {letter: "m",
+  translation: "--"},
+  {letter: "n",
+    translation: },
+  {letter: "o",
+  translation: },
+  {letter: "p",
+  translation: },
+  {letter: "q", 
+  translation: },
+  {letter: "r",
+  translation: },
+  {letter: "s",
+  translation: },
+  {letter: "t",
+  translation: },
+  {letter: "u", 
+  translation: },
+  {letter: "v",
+  translation: },
+  {letter: "w",
+  translation: },
+  {letter: "x",
+  translation: },
+  {letter: "y", 
+  translation: },
+  {letter: "z",
+    translation: }, 
 
 ]
+
+const morseCodeToLetters = [
+  {letter: ".-",
+  translation: "a" },
+  {letter:
+    translation: },
+  {letter:
+  translation: },
+  {letter:
+  translation: },
+  {letter: 
+  translation: },
+  {letter:
+    translation: },
+  {letter:
+  translation: },
+  {letter:
+  translation: },
+  {letter: 
+  translation: },
+  {letter:
+    translation: },
+  {letter:
+  translation: },
+  {letter:
+  translation: },
+  {letter: 
+  translation: },
+  {letter:
+    translation: },
+  {letter:
+  translation: },
+  {letter:
+  translation: },
+  {letter: 
+  translation: },
+  {letter:
+  translation: },
+  {letter:
+  translation: },
+  {letter:
+  translation: },
+  {letter: 
+  translation: },
+  {letter:
+  translation: },
+  {letter:
+  translation: },
+  {letter:
+  translation: },
+  {letter: 
+  translation: },
+  {letter:
+    translation: }, 
+
+]
+
