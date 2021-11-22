fa13e31d3a6327a79c3b866c185179ea74f0bf19
Julia Banerjee <juliabanerjee@Julias-MacBook-Pro.local>
2021-11-22 14:56:05 +0000

    got some JS but not quite working yet

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
-import {letterToMorseCode} from data.js;
+import {letterToMorseCode} from "./data.js";
 
 // allow text inputted to be saved as variable
 // event listener when enter is pressed
 // the answer to display on p tag
+
+const enterButton = document.querySelector("#enter")
+
+const translationDisplay = document.querySelector("#translationDisplay")
+
+const textbox = document.querySelector("#textbox")
+
+enterButton.addEventListener = ("click", (event) => {
+const saveInput = textbox.value
+
+if(saveInput == letterToMorseCode[i].letter){
+  const translation = letterToMorseCode[i].translation;
+
+  return translation
+displayingTranslation ()
+}
+})
+
+const displayingTranslation = () => {
+  
+  translationDisplay.innerHTML = translation
+
+
+}
 
 
 
