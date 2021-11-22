6330f194c639650acf1d5b812ac09eba522bf902
Julia Banerjee <juliabanerjee@Julias-MacBook-Pro.local>
2021-11-22 14:15:04 +0000

    html created, starting JS now

--- main.js
+++ main.js
@@ -1,112 +1,65 @@
-const letterToMorseCode = [
-  {letter: "a",
-  translation: ".-" },
-  {letter: "b",
-  translation: "-..." },
-  {letter: "c",
-  translation: "-.-." },
-  {letter: "d",
-  translation:  "-.." },
-  {letter: "e",
-  translation: "."},
-  {letter: "f",
-    translation: "..-."},
-  {letter: "g",
-  translation: "--."},
-  {letter: "h",
-  translation: "...."},
-  {letter: "i",
-  translation: ".."},
-  {letter: "j",
-    translation: ".---"},
-  {letter: "k",
-  translation: "-.-"},
-  {letter: "l",
-  translation: ".-.."},
-  {letter: "m",
-  translation: "--"},
-  {letter: "n",
-    translation: "-."},
-  {letter: "o",
-  translation: "---"},
-  {letter: "p",
-  translation: ".--."},
-  {letter: "q", 
-  translation: "--.-"},
-  {letter: "r",
-  translation: ".-."},
-  {letter: "s",
-  translation: "..."},
-  {letter: "t",
-  translation: "-"},
-  {letter: "u", 
-  translation: "..-"},
-  {letter: "v",
-  translation: "...-"},
-  {letter: "w",
-  translation: ".--"},
-  {letter: "x",
-  translation: "-..-"},
-  {letter: "y", 
-  translation: "-.--"},
-  {letter: "z",
-    translation:  "--.."}, 
+import {letterToMorseCode} from data.js;
 
-]
+// allow text inputted to be saved as variable
+// event listener when enter is pressed
+// the answer to display on p tag
 
-const morseCodeToLetters = [
-  {letter: ".-",
-  translation: "a" },
-  {letter:
-    translation: },
-  {letter:
-  translation: },
-  {letter:
-  translation: },
-  {letter: 
-  translation: },
-  {letter:
-    translation: },
-  {letter:
-  translation: },
-  {letter:
-  translation: },
-  {letter: 
-  translation: },
-  {letter:
-    translation: },
-  {letter:
-  translation: },
-  {letter:
-  translation: },
-  {letter: 
-  translation: },
-  {letter:
-    translation: },
-  {letter:
-  translation: },
-  {letter:
-  translation: },
-  {letter: 
-  translation: },
-  {letter:
-  translation: },
-  {letter:
-  translation: },
-  {letter:
-  translation: },
-  {letter: 
-  translation: },
-  {letter:
-  translation: },
-  {letter:
-  translation: },
-  {letter:
-  translation: },
-  {letter: 
-  translation: },
-  {letter:
-    translation: }, 
 
-]
+
+
+// const morseCodeToLetters = [
+//   {letter: ".-",
+//   translation: "a" },
+//   {letter:
+//     translation: },
+//   {letter:
+//   translation: },
+//   {letter:
+//   translation: },
+//   {letter: 
+//   translation: },
+//   {letter:
+//     translation: },
+//   {letter:
+//   translation: },
+//   {letter:
+//   translation: },
+//   {letter: 
+//   translation: },
+//   {letter:
+//     translation: },
+//   {letter:
+//   translation: },
+//   {letter:
+//   translation: },
+//   {letter: 
+//   translation: },
+//   {letter:
+//     translation: },
+//   {letter:
+//   translation: },
+//   {letter:
+//   translation: },
+//   {letter: 
+//   translation: },
+//   {letter:
+//   translation: },
+//   {letter:
+//   translation: },
+//   {letter:
+//   translation: },
+//   {letter: 
+//   translation: },
+//   {letter:
+//   translation: },
+//   {letter:
+//   translation: },
+//   {letter:
+//   translation: },
+//   {letter: 
+//   translation: },
+//   {letter:
+//     translation: }, 
+
+// ]
 
